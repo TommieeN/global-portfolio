497ad742dd6c165bb0244b99874b4e0ada1f224d
TommyNguyen <timsnguyen@hotmail.com>
2023-08-25 08:01:41 -0700

    navbar finish w/hero image

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,8 +1,10 @@
 import "./landingPage.scss";
+import hero from "../../assets/introduction-visual.png"
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
+        <img className="landing-page__hero" src={hero}/>
         <h1 className="landing-page__header">Your next interactive experience</h1>
         <button>HIRE US</button>
     </div>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,16 @@
-import "./navBar.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
+
 import NavLogo from "../../assets/logo.png";
 
+import "./navBar.scss";
+
+import { useState } from "react";
+
 const NavBar = () => {
+  const [hamburgerMenuOpen, setHamburgerMenuOpen] = useState(false);
+  const navBarLinks = ["Home", "Works", "About", "Contact"];
+
   return (
     <nav className="nav-bar">
       <div className="nav-bar__container">
@@ -11,7 +18,35 @@ const NavBar = () => {
         <p className="nav-bar__logo-text">GLOBAL</p>
       </div>
       <div>
-        <FontAwesomeIcon icon={faBars} size="lg" style={{ color: "#ffffff" }} />
+        <a
+          onClick={() => {
+            setHamburgerMenuOpen(!hamburgerMenuOpen);
+          }}
+          href="#"
+        >
+          {hamburgerMenuOpen ? (
+            <FontAwesomeIcon icon={faX} />
+          ) : (
+            <FontAwesomeIcon
+              icon={faBars}
+              size="lg"
+              style={{ color: "#ffffff" }}
+            />
+          )}
+        </a>
+        {hamburgerMenuOpen && (
+          <ul className="nav-bar__links">
+            {navBarLinks.map((navText) => {
+              return (
+                <a href="#">
+                  <li key={navText} className="nav-bar__link">
+                    {navText}
+                  </li>
+                </a>
+              );
+            })}
+          </ul>
+        )}
       </div>
     </nav>
   );
