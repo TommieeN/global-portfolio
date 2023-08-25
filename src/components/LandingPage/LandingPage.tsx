5a5e1b6c5b227ef3a77ad4fe9cddfb179f456fbe
TommyNguyen <timsnguyen@hotmail.com>
2023-08-25 11:19:14 -0700

    landing page & navbar finish mobile

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,14 +1,25 @@
 import "./landingPage.scss";
-import hero from "../../assets/introduction-visual.png"
-
+import hero from "../../assets/introduction-visual.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 const LandingPage = () => {
   return (
     <div className="landing-page">
-        <img className="landing-page__hero" src={hero}/>
-        <h1 className="landing-page__header">Your next interactive experience</h1>
-        <button>HIRE US</button>
+      <div className="landing-page__container">
+        <img className="landing-page__hero" src={hero} />
+        <h1 className="landing-page__header">
+          Your next interactive experience
+        </h1>
+      </div>
+      <div className="landing-page__btn-container">
+        <button className="landing-page__btn">
+          <span>CONTACT ME
+          <FontAwesomeIcon size="lg" icon={faArrowRight} />
+          </span>
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
