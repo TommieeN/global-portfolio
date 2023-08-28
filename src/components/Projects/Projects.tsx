0f63e93a6c60a05932e067306ebb6fa62fc9589e
TommyNguyen <timsnguyen@hotmail.com>
2023-08-28 13:42:32 -0700

    remove redundant import

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,6 +1,5 @@
 import "./projects.scss";
 import { projects } from "../../constants";
-import gitLogo from "../../assets/github-alt.svg";
 
 const Projects = () => {
   return (
@@ -9,26 +8,19 @@ const Projects = () => {
         {projects.map((project) => {
           return (
             <li className="project__item-container">
-              <div>
-                <div className="project__title-container">
-                  <p>{project.title}</p>
+              <div>                 
                   <a
                     target="_blank"
                     className="project__links"
                     href={project.link}
                   >
-                    <img
-                      className="project__github-logo"
-                      src={gitLogo}
-                      alt="cat-github-logo"
-                    />
                   </a>
-                </div>
                 <img
                   className="project__image"
                   src={project.img}
                   alt={`${project.title}-project-image`}
                 />
+                <p>{project.title}</p>
                 <div className="project__description-container">
                   <p>{project.description}</p>
                   <a target="_blank" href={project.deploy}>
