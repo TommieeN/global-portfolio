cb5529e1a9c182d6284a45fdf70618fe5df34807
TommyNguyen <timsnguyen@hotmail.com>
2023-09-01 22:57:29 -0700

    animation for navbar

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,22 +1,23 @@
 import "./landingPage.scss";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const LandingPage = () => {
   return (
-    <div className="landing-page">
+    <div className="landing-page" id="home">
       <div className="landing-page__container">
-        <h1 className="landing-page__header">
-          Hello, I'm Tommy Nguyen.
-        </h1>
-        <h1 className="landing-page__header">I solve problems and create captivating
-          applications.</h1>
+        <h1 className="landing-page__header">Hi, I'm Tommy Nguyen.</h1>
+        <p className="landing-page__header">
+          I solve problems and create captivating applications.
+        </p>
         <p className="landing-page__text">
-          I'm a Full Stack Web Developer based in Surrey, British Columbia, Canada,
-          deriving immense satisfaction from solving intricate problems through
-          the art of coding!
+          I'm a Full Stack Web Developer based in Surrey, British Columbia,
+          Canada, deriving immense satisfaction from solving intricate problems
+          through the art of coding!
         </p>
       </div>
       <div className="landing-page__logo-container">
@@ -43,7 +44,9 @@ const LandingPage = () => {
         </a>
       </div>
       <div className="landing-page__btn-container">
-        <AnchorLink href="#contact" className="landing-page__btn">CONTACT ME</AnchorLink>
+        <AnchorLink href="#contact" className="landing-page__btn">
+          CONTACT ME
+        </AnchorLink>
       </div>
     </div>
   );
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,18 +3,60 @@ import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 
 import "./navBar.scss";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { navBarLinks } from "../../constants";
+import { motion, AnimatePresence } from "framer-motion";
 
 const NavBar = () => {
   const [hamburgerMenuOpen, setHamburgerMenuOpen] = useState(false);
+  const [currentScrollPos, setCurrentScrollPos] = useState(0);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(false);
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+    exit: {
+      opacity: 0,
+      height: 0,
+      transition: {
+        ease: "easeIngOut",
+        duration: 0.3,
+        delay: 0.9,
+      },
+    },
+  };
+
+  // Hide navbar on scroll down, display on scroll up
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPos = window.scrollY;
+      setCurrentScrollPos(scrollPos);
+
+      if (scrollPos > prevScrollPos) {
+        setVisible(false);
+      } else {
+        setVisible(true);
+      }
+
+      setPrevScrollPos(scrollPos);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
 
   return (
-    <nav className="nav-bar" id="home">
-      <div className="nav-bar__container">
+    <nav className={`nav-bar ${visible ? "" : "nav-bar--hidden"}`}>
+      <header className="nav-bar__container">
         <h3 className="nav-bar__logo-text">Tommy</h3>
-      </div>
+      </header>
       <div>
         <button
           className="nav-bar__hamburger"
@@ -36,19 +78,39 @@ const NavBar = () => {
             />
           )}
         </button>
-        {hamburgerMenuOpen && (
-          <ul className="nav-bar__links">
-            {navBarLinks.map((navText) => {
-              return (
-                <AnchorLink key={navText.title} href={navText.anchorTag}>
-                  <li className="nav-bar__link">
-                    {navText.title}
-                  </li>
-                </AnchorLink>
-              );
-            })}
-          </ul>
-        )}
+        {/* <AnimatePresence> */}
+          {hamburgerMenuOpen && (
+            <motion.ul
+              variants={item}
+              initial={{ height: 0 }}
+              animate={{ height: 194 }}
+              transition={{ delayChildren: 15, staggerChildren: 11 }}
+              exit="exit"
+              className="nav-bar__links"
+            >
+              {navBarLinks.map((navText) => {
+                return (
+                  <AnchorLink
+                    onClick={() => {
+                      setHamburgerMenuOpen(false);
+                    }}
+                    key={navText.title}
+                    offset="80"
+                    href={navText.anchorTag}
+                  >
+                    <motion.li
+                      initial={{ scale: 0 }}
+                      animate={{ scale: 1 }}
+                      className="nav-bar__link"
+                    >
+                      {navText.title}
+                    </motion.li>
+                  </AnchorLink>
+                );
+              })}
+            </motion.ul>
+          )}
+        {/* </AnimatePresence> */}
       </div>
     </nav>
   );
